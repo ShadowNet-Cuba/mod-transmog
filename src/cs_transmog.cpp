3e0d23c90f26c368ffd5e89b71dc032b08b4f636
ShadowNet <tony931023@gmail.com>
2022-04-08 02:54:57 -0400

    Comando de agregar trasmutacion

--- src/cs_transmog.cpp
+++ src/cs_transmog.cpp
@@ -30,7 +30,13 @@ public:
 
     ChatCommandTable GetCommands() const override
     {
-        static ChatCommandTable commandTable =
+        static ChatCommandTable HandleDisableTransMogVisual =
+        {
+            { "add",        	HandleAddCommand,         SEC_GAMEMASTER,    Console::No },
+            { "delete",       	HandleDeleteCommand,      SEC_GAMEMASTER,    Console::No },
+            
+        };
+		static ChatCommandTable commandTable =
         {
             { "transmog", HandleDisableTransMogVisual, SEC_PLAYER, Console::No },
         };
@@ -56,6 +62,86 @@ public:
         player->UpdateObjectVisibility();
         return true;
     }
+	
+	static bool HandleAddCommand(ChatHandler* handler, char const* args)
+    {
+        Player* me = handler->GetSession()->GetPlayer();
+		
+		if (!*args)
+        {	
+			handler->SendSysMessage(LANG_IMPROPER_VALUE);
+            handler->SetSentErrorMessage(true);
+			ChatHandler(me->GetSession()).PSendSysMessage("Escriba el id del item a enviar");
+            return false;
+        }
+		
+		uint32 itemId = 0;
+		
+		if (args[0] == '[')                                        // [name] manual form
+        {
+            char const* itemNameStr = strtok((char*)args, "]");
+
+            if (itemNameStr && itemNameStr[0])
+            {
+                std::string itemName = itemNameStr + 1;
+
+                WorldDatabasePreparedStatement* stmt = WorldDatabase.GetPreparedStatement(WORLD_SEL_ITEM_TEMPLATE_BY_NAME);
+                stmt->setString(0, itemName);
+                PreparedQueryResult result = WorldDatabase.Query(stmt);
+
+                if (!result)
+                {
+                    handler->PSendSysMessage(LANG_COMMAND_COULDNOTFIND, itemNameStr + 1);
+                    handler->SetSentErrorMessage(true);
+                    return false;
+                }
+                itemId = result->Fetch()->GetUInt32();
+            }
+            else
+                return false;
+        }
+        else                                                    // item_id or [name] Shift-click form |color|Hitem:item_id:0:0:0|h[name]|h|r
+        {
+            char const* id = handler->extractKeyFromLink((char*)args, "Hitem");
+            if (!id)
+                return false;
+            itemId = uint32(atol(id));
+        }
+		
+		Player* player = handler->GetSession()->GetPlayer();
+        Player* playerTarget = handler->getSelectedPlayer();
+        if (!playerTarget)
+            playerTarget = player;
+
+        ItemTemplate const* itemTemplate = sObjectMgr->GetItemTemplate(itemId);
+        if (!itemTemplate)
+        {
+            handler->PSendSysMessage(LANG_COMMAND_ITEMIDINVALID, itemId);
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+		
+		QueryResult result = CharacterDatabase.Query("SELECT `account_id` = '{}' FROM `custom_unlocked_appearances` WHERE `item_template_id` = '{}'", playerTarget, itemId);
+		
+		if (player == playerTarget)
+		{
+			if (result)
+			{
+				ChatHandler(me->GetSession()).PSendSysMessage("Ya el jugador tiene el Item: |cff4CFF00 %s|r", itemId.c_str());
+			}
+			
+			else
+			{				
+				CharacterDatabase.Query("INSERT INTO `custom_unlocked_appearances` (`account_id`,`item_template_id`) VALUES ('{},{}')", playerTarget, itemId);
+				ChatHandler(me->GetSession()).PSendSysMessage("Agregada: |cff4CFF00 %s|r El Item", itemId);
+			}
+			
+        return true;
+		
+		}		
+		
+    }
+	
 };
 
 void AddSC_transmog_commandscript()
